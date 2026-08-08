2a15bef5e0d3fd9df59ee91d90cc76b03f5b5ad9
William <tanshawnwilliam@gmail.com>
2026-08-08 23:35:17 +0800

    changing path in session

--- app/Configuration/Admin/session.php
+++ app/Configuration/Admin/session.php
@@ -41,7 +41,7 @@ function isAdminLoggedIn(): bool
 function requireAdminAuth(): void
 {
     if (!isAdminLoggedIn()) {
-        header("Location: ../views/Admin/adm-dashboard.php");
+        header("Location: /admin/adm-dashboard");
         exit;
     }
 
